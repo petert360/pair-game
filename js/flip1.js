63038c604fd6f5b14da98cc5ab434647307edf32
petert360 <peter.t360@hotmail.com>
2020-12-05 12:02:26 +0100

    File rename, new flip test (flip2.html)

--- js/flip1.js
+++ js/flip1.js
@@ -1,8 +1,9 @@
-//https://w3bits.com/css-flip-animation/
+let isCardFlipped = false;
 
+//https://w3bits.com/css-flip-animation/
 let cardElements = document.querySelectorAll('.card__inner');
 Array.from(cardElements).forEach((card) => {
     card.addEventListener('click', () => {
-        card.classList.toggle('flipCard');
+        card.classList.add('flippedCard');
     })
 })  
---
+++ js/flip2.js
@@ -0,0 +1,9 @@
+let isCardFlipped = false;
+
+//https://w3bits.com/css-flip-animation/
+let cardElements = document.querySelectorAll('.card__inner');
+Array.from(cardElements).forEach((card) => {
+    card.addEventListener('click', () => {
+        card.classList.add('flippedCard');
+    })
+})  
