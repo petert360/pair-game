58754c44c3bed064a8d1aa7162d5c1b6b5511a15
petert360 <peter.t360@hotmail.com>
2020-12-05 14:24:05 +0100

    Add shuffle argorithm

--- js/flip1.js
+++ js/flip1.js
@@ -6,4 +6,22 @@ Array.from(cardElements).forEach((card) => {
     card.addEventListener('click', () => {
         card.classList.add('flippedCard');
     })
-})  
+})
+
+//Fisher-Yates Shuffle algoritm
+//https://medium.com/@joshfoster_14132/best-javascript-shuffle-algorithm-c2c8057a3bc1
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+};
+
+const sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(sampleArray)
+console.log(shuffle(sampleArray));
